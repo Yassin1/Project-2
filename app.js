7eaa81fe8aa374183757ff87b357f59552801693
yassin alisheikh <yassinnawed@gmail.com>
2021-12-20 09:48:29 -0800

    deployed on heroku

--- app.js
+++ app.js
@@ -95,18 +95,3 @@ app.post('/api/signin', (req, res) => {
     });
 });
 
-let sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
